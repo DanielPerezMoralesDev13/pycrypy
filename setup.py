5d7d89460f382eb415e6d5e0d002f5ed749d06d9
d4nitrix13 <danielperezdev@proton.me>
2024-08-26 20:09:37 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -8,38 +8,36 @@ from io import TextIOWrapper
 
 f: Optional[TextIOWrapper] = None
 
-with open(file = r"README.md", mode = "r", encoding = "utf-8") as f:
+with open(file=r"README.md", mode="r", encoding="utf-8") as f:
     README: str = f.read()
 
 setup(
-    name = "pycrypy",
-    version = "0.0.0.dev1",
-    author = "Daniel Benjamin Perez Morales",
-    author_email = "danielperezdev@proton.me",
-    description = "Esta utilidad, desarrollada en Python3, simplifica significativamente el proceso de configuracion de Alacritty, permitiendo ajustar de manera simple la fuente, el tema, el padding, los cursores y los estilos de la fuente.",
-    long_description = README,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/DanielPerezMoralesDev13/pycrypy.git",
-    packages = find_packages(where = "src"),
-    package_dir = {"": "src"},  # Indica que los paquetes están en "src"
+    name="pycrypy",
+    version="0.0.0.dev1",
+    author="Daniel Benjamin Perez Morales",
+    author_email="danielperezdev@proton.me",
+    description="Esta utilidad, desarrollada en Python3, simplifica significativamente el proceso de configuracion de Alacritty, permitiendo ajustar de manera simple la fuente, el tema, el padding, los cursores y los estilos de la fuente.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanielPerezMoralesDev13/pycrypy.git",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},  # Indica que los paquetes están en "src"
     license="MIT",
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
-        "Topic :: Terminals :: Terminal Emulators/X Terminals"
+        "Topic :: Terminals :: Terminal Emulators/X Terminals",
     ],
-    keywords = "alacritty",
-    python_requires = ">=3.6",
-    install_requires = [
-        "pytoml >= 0.1.21",
+    keywords="alacritty",
+    python_requires=">=3.6",
+    install_requires=[
+        "toml >= 0.10.2",
         "colored >= 2.2.4",
         "mypy >= 1.10.0",
         "pyinstaller >= 6.6.0",
         "prettytable >= 3.10.2",
     ],
-    include_package_data = True,
-    entry_points = {
-        "console_scripts": ["pycrypy = cli.main:main"]
-    }
+    include_package_data=True,
+    entry_points={"console_scripts": ["pycrypy = cli.main:main"]},
 )
